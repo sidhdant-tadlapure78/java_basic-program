b806b9e517dba028c2f46b7463e3c206a56174b4
sidhdant-tadlapure78 <stadlapure78@gmail.com>
2026-01-21 16:47:05 +0530

    add java array file

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    
+    public static void main(String[] args) {
+        int arr[]={10,43,56,3,81};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        
+    }
+}
