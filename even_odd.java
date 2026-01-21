152d903d0af5b5779d86910c1c1e48cfee542fa2
sidhdant-tadlapure78 <stadlapure78@gmail.com>
2026-01-21 16:14:16 +0530

    add new java file

---
+++ even_odd.java
@@ -0,0 +1,13 @@
+class even_odd 
+{
+	public static void main(String[] args) 
+	{
+		int n=34;
+		if(n/2==0){
+		System.out.println("The no is even no");
+	}
+	else{
+		System.out.println("The no is odd");
+	}
+	}
+}
